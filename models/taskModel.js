a61a551f3dcfb753bca4c1e684c5070453999b97
tusshar17 <tr.jakhara019@gmail.com>
2025-01-19 13:36:44 +0530

    user ref added in task model

--- models/taskModel.js
+++ models/taskModel.js
@@ -6,6 +6,7 @@ const taskSchema = new mongoose.Schema(
         title: { type: String, required: true },
         description: { type: String, required: false},
         status: { type: Boolean, required: true },
+        userID: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true}
     },
     {
         timestamps: true,
